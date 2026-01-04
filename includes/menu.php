1f4ef38ba9db8549d6fa39e223191ab52f4db6c6
Christian <christianrodesvidal@gmail.com>
2026-01-04 19:15:15 +0100

    estructura final final

--- includes/menu.php
+++ includes/menu.php
@@ -2,6 +2,13 @@
     <a href="/tienda-publica/public/index.php">Inicio</a> |
     <a href="/tienda-publica/public/categorias.php">Categorías</a> |
     <a href="/tienda-publica/public/contacto.php">Contacto</a> |
-    <a href="/tienda-publica/public/register.php">Registrarse</a> |
-    <a href="/tienda-publica/public/login.php">Login</a>
+
+    <?php if (isset($_SESSION['usuario'])): ?>
+        Hola <strong><?= htmlspecialchars($_SESSION['usuario']['nombre']) ?></strong>
+        (<?= $_SESSION['usuario']['rol'] ?>) |
+        <a href="/tienda-publica/public/logout.php">Cerrar sesión</a>
+    <?php else: ?>
+        <a href="/tienda-publica/public/register.php">Registrarse</a> |
+        <a href="/tienda-publica/public/login.php">Login</a>
+    <?php endif; ?>
 </nav>
